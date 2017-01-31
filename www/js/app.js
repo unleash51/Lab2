c9c7165f9e8567032eaad0b4b8cd8a2e8eb996d9
unleash51 <renterjohnaride@gmail.com>
2017-02-01 07:10:22 +0800

    Creating a calendar view

--- www/js/app.js
+++ www/js/app.js
@@ -59,12 +59,51 @@ angular.module('starter', ['ionic'])
         }
       }
     })
+    .state('tabs.calendar',{
+      url: '/calendar',
+      views: {
+        'calendar-tab' : {
+          templateUrl: 'templates/calendar.html',
+          controller: 'CalendarController'
+        }
+      }
+    })
     $urlRouterProvider.otherwise('/tab/home');
 })
 
+.controller('CalendarController', ['$scope', '$http', '$state',function($scope, $http, $state){
+  $http.get('js/data.json').success(function(data){
+    $scope.calendar = data.calendar;
+    
+    $scope.onItemDelete = function(dayIndex, item){
+      $scope.calendar[dayIndex].schedule.splice($
+        scope.calendar[dayIndex].schedule
+        .indexOf(item), 1);
+    }
+
+    $scope.doRefresh = function(){
+      $http.get('js/data.json').success(function(
+      data){
+        $scope.calendar = data.calendar;
+        $scope.$broadcast('scroll.refreshComplete'
+        );
+      });
+    } 
+
+    $scope.toggleStar = function(item){
+      item.star = !item.star;
+    }
+
+    $scope.moveItem = function(item, fromIndex, 
+      toIndex){
+      $scope.artists.splice(fromIndex, 1);
+      $scope.artists.splice(toIndex, 0, item);
+    };
+  })
+
 .controller('ListController', ['$scope', '$http', '$state',function($scope, $http, $state){
   $http.get('js/data.json').success(function(data){
-    $scope.artists = data;
+    $scope.artists = data.artists;
     $scope.whichartist=$state.params.aId
     $scope.data = {showDelete: false, showReorder: false};
 
